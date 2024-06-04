2d942877bba5f82c3e2ca50ab48aa7c52b004be6
Nastya Gracheva <nastya.nastya.gracheva.98.grach@mail.ru>
2024-06-04 19:45:52 +0300

    origin

---
+++ src/main/java/org/example/Handler.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
+
+
+public interface Handler {
+    public void handle(Request request, BufferedOutputStream responseStream) throws IOException;
+}
